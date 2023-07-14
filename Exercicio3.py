b27d33077b6bde16aeda575cb8f579dea6e27953
Matheus Felix <matheusfelix.amorim@hotmail.com>
2023-07-14 17:14:12 -0300

    Add files via upload

---
+++ Exercicio3.py
@@ -0,0 +1,7 @@
+print('====== DESAFIO 03 ======')
+numero1 = input('Primeiro numero ')
+numero2 = input('Segundo numero ')
+numero1_int = int(numero1)
+numero2_int = int(numero2)
+soma = numero1_int + numero2_int
+print('A soma é ', soma)
